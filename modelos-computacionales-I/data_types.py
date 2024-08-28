1d3ed3e7b02a5750763a3ed0b9c567b9188b69a7
aitanaol <aitana@comunidad.unam.mx>
2024-08-28 11:19:23 -0600

    Tareas 2 y 3

---
+++ modelos-computacionales-I/data_types.py
@@ -0,0 +1,12 @@
+# Tipos de datos boleanos
+
+valor1 = True
+valor2 = False
+
+print("tipos de valores boleanos:", valor1, valor2)
+print("usando type:", type(valor1), type(valor2))
+
+print("usando and", valor1 and valor2)
+print("usando or", valor1 or valor2)
+
+# 
